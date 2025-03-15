2620d1141187eef2f8130c75fec6aff9678629a2
ASHUTOSH PAWAR <pawarash000@outlook.com>
2025-03-15 11:39:17 +0530

    UPDATED: Some changes

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { CONNECT_DATABASE } from "./config/db/index.js";
 import { MONGO_URI, PORT } from "./config/env/index.js";
-import { typeDefs } from "./schemas/typedefs.schema.js";
-import { resolvers } from "./schemas/resolvers.schema.js";
+import { typeDefs } from "./graphql/schemas/typedefs.schema.js";
+import { resolvers } from "./graphql/schemas/resolvers.schema.js";
 
 const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 async function startServer() {
     await server.start();
-    server.applyMiddleware({ app });
+    server.applyMiddleware({ app, path:"/graphql" });
     CONNECT_DATABASE(MONGO_URI);
     app.listen(PORT, () => {
         console.log(
